680d8a9227fb115ffc77833ed6cf62221dd9e159
Balbir Yadav <balbir.iitbombay3602@gmail.com>
2022-06-08 22:15:36 +0530

    frontend initial commit

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MoviesComponent } from './movies/movies.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MoviesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
